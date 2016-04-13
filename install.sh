3caf9d3d24ce9a528873267d860bf338f9f024ae
catonrug <aigars.kadikis@gmail.com>
2016-04-13 21:48:22 +0300

    next

--- install.sh
+++ install.sh
@@ -1,20 +1,31 @@
+#this is tested on fresh 2016-03-18-raspbian-jessie-lite.img
+
+#move to supper user
 #sudo su
+
+#move to hemo direcotry
 #cd
 
 #clone repo
-#git clone https://github.com/catonrug/raspbian-icecast2-mpd-mpc.git && cd raspbian-icecast2-mpd-mpc && chmod +x install.sh
-#time ./install.sh
+#git clone https://github.com/catonrug/raspbian-icecast2-mpd-mpc.git
+#cd raspbian-icecast2-mpd-mpc
+#chmod +x install.sh
+#./install.sh
 
 #update the system
 apt-get update -y && apt-get upgrade -y
 
+#set up correct time zone
 cp /usr/share/zoneinfo/Etc/GMT-2 /etc/localtime
-
 
 #install music player daemon
 apt-get install mpd -y
 
+#put IP address into the variable
 ipaddress=$(ifconfig | grep "inet.*cast" | sed "s/[: ]/\n/g" | grep -m1 "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+")
+
+#set mpd configuration file. you can change 'hackme' password.
+#make sure you type the same password as in icecast configuration
 cat > /etc/mpd.conf << EOF
 music_directory         "/var/lib/mpd/music"
 playlist_directory              "/var/lib/mpd/playlists"
@@ -50,8 +61,10 @@ EOF
 #install music player controller
 apt-get install mpc -y
 
+#install samba share
 apt-get install samba samba-common-bin -y
 
+#set up samba share public share which will be available without any password 
 cat > /etc/samba/smb.conf << EOF
 #======================= Global Settings =========================
 [global]
@@ -67,21 +80,24 @@ guest ok = yes
 read only = no
 EOF
 
-#set permissions
+#any user from any system can write into this samba share
 chmod -R 777 /var/lib/mpd/music
 chmod -R g+s /var/lib/mpd/music
 chmod g+w /var/lib/mpd/music /var/lib/mpd/playlists
 chgrp audio /var/lib/mpd/music /var/lib/mpd/playlists
 
+#restart sharing service
 /etc/init.d/samba restart
 
-#icecast is streaming software that puts music into the air
+#install icecast2 which will host our mp3 stream into the air
+#you will be prompted to set up icecast server. shoose [yes]
 apt-get install icecast2 -y
 
 #modprobe
 modprobe ipv6
 sleep 1
 
+#restart music player daemon service
 /etc/init.d/mpd restart
 sleep 1
 
@@ -107,18 +123,20 @@ mpc ls | mpc add
 #set shufle on
 mpc random on
 
-#set shufle on
+#set repeat on
 mpc repeat on
 
 #show playlist
 mpc playlist
 
-#start stream
+#start stream instantly
 mpc play
 
 #retry the service if not running every minute
 echo "* * * * * root mpc play">> /etc/crontab
+
+#auto refresh playlist at midnight
 echo "00 00 * * * root mpc stop && mpc clear && mpc update && mpc ls | mpc add && mpc random on && mpc repeat on">> /etc/crontab
+
+#restart sheduled task service
 /etc/init.d/cron restart
-
-
