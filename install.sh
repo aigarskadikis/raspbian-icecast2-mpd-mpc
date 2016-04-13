6fc6e3ada07e39ce4029dd0d12da96ca65d336d0
catonrug <aigars.kadikis@gmail.com>
2016-04-13 20:00:12 +0300

    next

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #move to supper user
 #sudo su
 
+#cd
+
 #clone repo
 #git clone https://github.com/catonrug/raspbian-icecast2-mpd-mpc.git && cd raspbian-icecast2-mpd-mpc && chmod +x install.sh
 #time ./install.sh
@@ -8,26 +10,51 @@
 #update the system
 apt-get update -y && apt-get upgrade -y
 
-#copy Media Player Control configuration
-cp mpd.conf /etc
-#make sure permissions are ok
-chmod 640 /etc/mpd.conf
-#change password
-sed -i "s/F2crCrRQ/newpassword/g" /etc/mpd.conf
-
-#create direcotry for icecast2 configuration
-mkdir -p /etc/icecast2
-#copy icecast configuration
-cp icecast.xml /etc/icecast2
-chmod 660 /etc/icecast2/icecast.xml
-#change password
-sed -i "s/F2crCrRQ/newpassword/g" /etc/icecast2/icecast.xml
-
 #install music player daemon
 apt-get install mpd -y
 
+ipaddress=$(ifconfig | grep "inet.*cast" | sed "s/[: ]/\n/g" | grep -m1 "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+")
+cat > /etc/mpd.conf << EOF
+music_directory         "/var/lib/mpd/music"
+playlist_directory              "/var/lib/mpd/playlists"
+db_file                 "/var/lib/mpd/tag_cache"
+log_file                        "/var/log/mpd/mpd.log"
+pid_file                        "/run/mpd/pid"
+state_file                      "/var/lib/mpd/state"
+sticker_file                   "/var/lib/mpd/sticker.sql"
+user                            "mpd"
+bind_to_address         "127.0.0.1"
+
+input {
+plugin "curl"
+}
+
+audio_output {
+type            "shout"
+name            "Algorithm"
+description     "MPD stream on Raspberry Pi"
+host            "$ipaddress"
+port            "8000"
+mount           "/mpd"
+password        "hackme"
+bitrate         "128"
+format          "44100:16:2"
+encoding        "mp3"
+}
+
+filesystem_charset              "UTF-8"
+id3v1_encoding                  "UTF-8"
+EOF
+
 #install music player controller
 apt-get install mpc -y
+
+#copy icecast2 configuration 
+cp -a icecast2 /etc
+chmod 775 -R /etc/icecast2
+chmod 660 /etc/icecast2/*
+chmod 664 /etc/icecast2/admin/*
+chmod 775 /etc/icecast2/web/*
 
 #icecast is streaming software that puts music into the air
 apt-get install icecast2 -y
@@ -36,6 +63,7 @@ apt-get install icecast2 -y
 modprobe ipv6
 
 /etc/init.d/mpd restart
+/etc/init.d/icecast2 restart
 
 #check output
 mpc outputs
@@ -47,5 +75,21 @@ netstat -ltpn
 #set permissions
 chmod g+w /var/lib/mpd/music/ /var/lib/mpd/playlists/
 chgrp audio /var/lib/mpd/music/ /var/lib/mpd/playlists/
+
+#clear playlist
+mpc clear
+
+#scan /var/lib/mpd/music for new songs
+mpc update
+
+#list all songs in /var/lib/mpd/music and add it to playlist
+mpc ls | mpc add
+
+#show playlist
+mpc playlist
+
+#start stream
+mpc play
+
 
 
