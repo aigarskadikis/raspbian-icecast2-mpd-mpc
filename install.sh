edf875822b10229d5c19d43c02b304babeb81ea0
catonrug <aigars.kadikis@gmail.com>
2016-04-13 18:54:23 +0300

    next

--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 #move to supper user
-sudo su
+#sudo su
 
 #update the system
 apt-get update -y && apt-get upgrade -y
+
+#copy Media Player Control configuration
+cp mpd.conf /etc
+#make sure permissions are ok
+chmod 640 /etc/mpd.conf
+#change password
+sed -i "s/F2crCrRQ/newpassword/g" /etc/mpd.conf
+
+#create direcotry for icecast2 configuration
+mkdir -p /etc/icecast2
+#copy icecast configuration
+cp icecast.xml /etc/icecast2
+chmod 660 /etc/icecast2/icecast.xml
+#change password
+sed -i "s/F2crCrRQ/newpassword/g" /etc/icecast2/icecast.xml
 
 #install music player daemon
 apt-get install mpd -y
@@ -16,38 +31,7 @@ apt-get install icecast2 -y
 #modprobe
 modprobe ipv6
 
-/etc/mpd.conf
-
-music_directory         "/var/lib/mpd/music"
-playlist_directory              "/var/lib/mpd/playlists"
-db_file                 "/var/lib/mpd/tag_cache"
-log_file                        "/var/log/mpd/mpd.log"
-pid_file                        "/run/mpd/pid"
-state_file                      "/var/lib/mpd/state"
-sticker_file                   "/var/lib/mpd/sticker.sql"
-user                            "mpd"
-bind_to_address         "127.0.0.1"
-input {
-        plugin "curl"
-}
-audio_output {
- type            "shout"
- name            "RasPi MPD Stream"
- description     "MPD stream on Raspberry Pi"
- host            "192.168.88.86"
- port            "8000"
- mount           "/mpd.mp3"
- password        "hackme"
- bitrate         "128"
- format          "44100:16:2"
- encoding        "mp3"
-}
-filesystem_charset              "UTF-8"
-id3v1_encoding                  "UTF-8"
-
-
 /etc/init.d/mpd restart
-
 
 #check output
 mpc outputs
@@ -59,3 +43,5 @@ netstat -ltpn
 #set permissions
 chmod g+w /var/lib/mpd/music/ /var/lib/mpd/playlists/
 chgrp audio /var/lib/mpd/music/ /var/lib/mpd/playlists/
+
+
